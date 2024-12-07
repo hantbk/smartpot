4d234f87d6c3d25ba10e0e1b56a94af62f7a40ae
vanhao2310 <vanhao2310@gmail.com>
2024-12-07 12:35:05 +0700

    firebase config

--- plantapp/lib/main.dart
+++ plantapp/lib/main.dart
@@ -1,7 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:plantapp/pages/home/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      name: 'SecondaryApp',
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBpnuQTxAQrp4KliZLXfqO9bhlGSoLEmbA",
+          authDomain: "iot1-33b58.firebaseapp.com",
+          databaseURL:
+              "https://iot1-33b58-default-rtdb.asia-southeast1.firebasedatabase.app",
+          projectId: "iot1-33b58",
+          storageBucket: "iot1-33b58.firebasestorage.app",
+          messagingSenderId: "673389701956",
+          appId: "1:673389701956:web:ebabe049800319a7350511",
+          measurementId: "G-8M54J55R6T"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -11,9 +31,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home()
-    );
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: Home());
   }
 }
