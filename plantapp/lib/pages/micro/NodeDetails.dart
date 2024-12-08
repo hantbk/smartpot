9880ede04e1f092467a488b9c5c0643c0d556a29
AnTu <an106203h@gmail.com>
2024-12-08 23:12:12 +0700

    kfjf

--- plantapp/lib/pages/micro/NodeDetails.dart
+++ plantapp/lib/pages/micro/NodeDetails.dart
@@ -6,7 +6,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:plantapp/pages/PlantIndentifyPage.dart';
 import 'package:plantapp/pages/models/Plant.dart';
-import 'package:plantapp/pages/micro/semicircle_indicator.dart';
 import 'package:plantapp/pages/micro/SmartPlanting.dart';
 
 class NodeDetails extends StatefulWidget {
